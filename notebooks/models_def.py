a0b8b1bc9a3a03efd85c3821c9f9a11c3d4bf927
AloneGu <jackling.gu@gmail.com>
2018-03-01 15:07:41 +0800

    add lstm glove feat , improve xgb and blend res

--- notebooks/models_def.py
+++ notebooks/models_def.py
@@ -173,6 +173,21 @@ class Attention(Layer):
                  W_regularizer=None, b_regularizer=None,
                  W_constraint=None, b_constraint=None,
                  bias=True, **kwargs):
+        """
+        Keras Layer that implements an Attention mechanism for temporal data.
+        Supports Masking.
+        Follows the work of Raffel et al. [https://arxiv.org/abs/1512.08756]
+        # Input shape
+            3D tensor with shape: `(samples, steps, features)`.
+        # Output shape
+            2D tensor with shape: `(samples, features)`.
+        :param kwargs:
+        Just put it on top of an RNN Layer (GRU/LSTM/SimpleRNN) with return_sequences=True.
+        The dimensions are inferred based on the output shape of the RNN.
+        Example:
+            model.add(LSTM(64, return_sequences=True))
+            model.add(Attention())
+        """
         self.supports_masking = True
         self.init = initializers.get('glorot_uniform')
 
